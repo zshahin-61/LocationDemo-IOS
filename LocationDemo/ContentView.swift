d21233ae4badd29cdf38866527560ef51335f0c3
Jupiter <profjigisha@gmail.com>
2021-08-19 15:41:32 -0400

    Initial Commit

---
+++ LocationDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocationDemo
+//
+//  Created by Jigisha Patel on 2021-08-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocationDemo/LocationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationDemoApp.swift
+//  LocationDemo
+//
+//  Created by Jigisha Patel on 2021-08-19.
+//
+
+import SwiftUI
+
+@main
+struct LocationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
